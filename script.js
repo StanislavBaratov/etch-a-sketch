21802de2300c4a950228fc8d068b37d9d61bc557
StanislavBaratov <stanislavbaratov@mail.ru>
2025-03-10 23:11:06 +0500

    Add canvas creation logic

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-console.log('Hello');
+const PIXEL_SIZE = '20px';
+
+const clearWorkspace = function() {
+    const workspace = document.querySelectorAll('main div');
+    workspace.forEach((item) => item.remove());
+}
+
+const createPixel = function() {
+    const pixel = document.createElement('div');
+    pixel.style.backgroundColor = '#fff';
+    pixel.style.borderWidth = '1px';
+    pixel.style.borderColor = '#000';
+    pixel.style.borderStyle = 'solid';
+    pixel.style.flexGrow = 0;
+    pixel.style.flexShrink = 0;
+    pixel.style.flexBasis = PIXEL_SIZE;
+    pixel.style.width = PIXEL_SIZE;
+
+    return pixel;
+
+}
+
+const createCanvas = function(width, height) {
+    const canvas = document.createElement('div');
+    canvas.setAttribute('id', 'canvas');
+    
+    const rows = [];
+    for (let i = 0; i < height; i++) {
+        const row = document.createElement('div');
+        row.style.flexDirection = 'column';
+
+        for (let j = 0; j < width; j++) {
+            row.appendChild(createPixel());
+        }
+        canvas.appendChild(row);
+    }
+
+    return canvas;
+}
+
+const createNewImage = function() {
+    const workspace = document.querySelector('main');
+    clearWorkspace();
+    
+    workspace.appendChild(createCanvas(16, 16));
+}
 
 const createWelcomeText = function() {
     const welcomeText = document.createElement('div');
@@ -14,9 +59,11 @@ const createCreateButton = function() {
     createButton.setAttribute('id', 'create-button');
     createButton.setAttribute('type', 'button');
     createButton.textContent = 'Create image';
+    createButton.addEventListener('click', createNewImage);
 
     return createButton;
 }
+
 const initWorkspace = function() {
     const startBlock = document.createElement('div');
     startBlock.setAttribute('id', 'start');
